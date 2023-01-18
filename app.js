36054ed66603779498e59d14725d7c018d71252c
SzymanskiKarol <szymanski.karol.1991@gmail.com>
2023-01-18 23:25:15 +0100

    js added

--- app.js
+++ app.js
@@ -1,3 +1,96 @@
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const deleteButton = document.querySelector(".del-btn");
+const resultButton = document.querySelector(".result-btn");
+const resetButton = document.querySelector(".reset-btn");
+let resultNumber = document.getElementById("result");
+let firstNumber = document.getElementById("first-number");
+let mathSign = document.getElementById("math-sign");
+
+let result = '';
+
+// Functions
+function displayNumbers() {
+    if (this.textContent === '.' && resultNumber.innerHTML.includes('.')) return;
+    if (this.textContent === '.' && resultNumber.innerHTML === '') return resultNumber.innerHTML = '.0'
+
+    resultNumber.innerHTML += this.textContent;
+}
+
+function operate() {
+    if (resultNumber.innerHTML === '' && this.textContent === '-') {
+        resultNumber.innerHTML = '-';
+        return;
+    }
+    else if (resultNumber.innerHTML === '') {
+        return;
+    }
+
+    if (mathSign.innerHTML !== '') {
+        showResult();
+    }
+    firstNumber.innerHTML = resultNumber.innerHTML;
+    mathSign.innerHTML = this.textContent;
+    resultNumber.innerHTML = '';
+}
+
+function showResult() {
+    if (firstNumber.innerHTML === '' || resultNumber.innerHTML === '') return;
+
+    let a = Number(resultNumber.innerHTML);
+    let b = Number(firstNumber.innerHTML);
+    let operator = mathSign.innerHTML;
+
+    switch (operator) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = b - a;
+            break;
+        case 'x':
+            result = a * b;
+            break;
+        case '/':
+            result = b / a;
+            break;
+    }
+
+    resultNumber.innerHTML = result;
+    firstNumber.innerHTML = '';
+    mathSign.innerHTML = '';
+}
+
+function deleteLast() {
+    resultNumber.innerHTML = resultNumber.innerHTML.toString().slice(0, -1);
+}
+
+function resetScreen() {
+    result = '';
+    firstNumber.innerHTML = '';
+    resultNumber.innerText = '';
+    mathSign.innerText = '';
+}
+
+// Event Listeners
+numberButtons.forEach((button) => {
+    button.addEventListener('click', displayNumbers)
+});
+
+operatorButtons.forEach((operator) => {
+    operator.addEventListener('click', operate)
+});
+
+deleteButton.addEventListener('click', deleteLast);
+resultButton.addEventListener('click', showResult);
+resetButton.addEventListener('click', resetScreen);
+
+
+
+
+
+
+// THEME TOGGLE
 const themeBtn = document.querySelector(".theme-btn");
 const themeHtml = document.querySelector("html");
 
